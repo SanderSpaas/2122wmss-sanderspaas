5dc54bc10e653edb933cfc0aeed662bfd5bfef11
SanderSpaas <80250065+SanderSpaas@users.noreply.github.com>
2021-10-03 18:17:29 +0200

    Update opgave1.5.php
    opgave 1.5 afgewerkt

--- app/bin/labo01/opgave1.5.php
+++ app/bin/labo01/opgave1.5.php
@@ -6,13 +6,18 @@
  */
 
  $parameter = $argv[1];
- if (isPalindrome($parameter)){
+ $parameter2 = $argv[2]; 
+ /**if (isPalindrome($parameter)){
     echo $parameter . ' is a palindrome.';
  }
  else{
     echo $parameter . ' is not a palindrome.';
  }
 
+print_r(factuur($parameter, $parameter2) . 'Euro');
+*/
+$my_year = $parameter;
+year_check($my_year);
 
  function isPalindrome(string $string) : bool {
    return $string === strrev($string);
@@ -21,4 +26,42 @@
     }
     return false;
     */
+ }
+
+ //oefening 1.5b
+function year_check($my_year)
+{
+if ($my_year % 400 == 0){
+    print("It is a leap year");
+}
+if ($my_year % 4 == 0){
+   print("It is a leap year");
+}
+else if ($my_year % 100 == 0){
+   print("It is not a leap year");
+}
+else{
+   print("It is not a leap year");
+}
+
+}
+
+ //oefening 1.5C
+ function factuur(int $amount, int $amountOfKids) : float {
+   $bonus = 0;
+    if ($amountOfKids <= 2){
+       $bonus = 0.10;
+    }
+   if ($amount <= 50){
+      $bill = $amount * 0.15;
+      return $bill + ($bill * $bonus);
+   }
+   else if($amount <= 150){
+      $bill = ((50 * 0.15) + ($amount - 50) * 0.25);
+      return $bill + ($bill * $bonus);
+   }
+   else if($amount > 150){
+      $bill = (50 * 0.15) + ($amount - 50) * 0.25 + ($amount - 150) * 0.30;
+      return $bill + ($bill * $bonus);
+   }
  }
